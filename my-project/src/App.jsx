6e665bb06fb834bb194fe28980a7059d57de658d
AlexanderJarvheden <alexander.jarvheden@gmail.com>
2024-02-21 13:08:17 +0100

    Finish tailwindcss tutorial, shoutout to @Lukas | Web Development & Design

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+  return <div>
+    <section className="m-12 flex h-screen items-center justify-center bg-twitter-blue text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis adipisci repellat,
+        omnis quaerat necessitatibus eaque esse rerum ratione illo a eligendi voluptatum at
+        asperiores, alias, ducimus totam consequuntur expedita autem.</p>
+    </section>
+    <div className='card rounded-none'>This is a card.</div>
+  </div>;
+}
+
+export default App
